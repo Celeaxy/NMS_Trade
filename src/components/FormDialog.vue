5aae44142efd664bcda3bbeae50cdf49d8ad1235
Celeaxy <byt.es@mailbox.org>
2025-09-26 19:00:22 +0200

    Added FormDialog component

---
+++ src/components/FormDialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog
+    v-model="modelValue"
+    max-width="500"
+    @update:model-value="(val) => emit('update:modelValue', val)"
+  >
+    <v-card>
+      <v-card-title class="text-h6">{{ title }}</v-card-title>
+      <v-card-text>
+        <slot name="form" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="grey" variant="text" @click="cancel">Cancel</v-btn>
+        <v-btn color="primary" variant="flat" @click="submit">Submit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, ref } from 'vue';
+import {
+  VDialog,
+  VCard,
+  VCardTitle,
+  VCardText,
+  VCardActions,
+  VSpacer,
+  VBtn,
+} from 'vuetify/components';
+const modelValue = ref(true);
+defineProps({
+  title: {
+    type: String,
+    default: 'Form',
+  },
+});
+
+const emit = defineEmits(['update:modelValue', 'submit', 'cancel']);
+
+function cancel() {
+  emit('update:modelValue', false);
+  emit('cancel');
+}
+function submit() {
+  emit('submit');
+  emit('update:modelValue', false);
+}
+</script>
