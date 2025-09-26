d9d8a6a1865498e0d19c261141b54dde94a50e51
Celeaxy <byt.es@mailbox.org>
2025-09-26 14:23:12 +0200

    migrated item view to vuetify

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,28 +1,43 @@
 <template>
-  <div>
+  <v-sheet>
     <h1>Items</h1>
-    <form @submit.prevent="addItem">
-      <input v-model="newItemName" placeholder="New item name" />
-      <input v-model.number="newItemValue" type="number" placeholder="Value" />
-      <button type="submit">Add Item</button>
-    </form>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        {{ item.name }} (Value: {{ item.value }})
-        <button @click="removeItem(item.id)">Remove</button>
-      </li>
-    </ul>
-  </div>
+    <v-list>
+      <v-list-item
+        v-for="item in items"
+        :key="item.id"
+        :title="item.name"
+        :subtitle="'Value: ' + item.value"
+      >
+      <template v-slot:append>
+        <v-btn color="red" icon="mdi-delete" @click="removeItem(item.id)" variant="text"></v-btn>
+        </template>
+      </v-list-item>
+      <v-list-item>
+        <v-spacer> </v-spacer>
+      </v-list-item>
+    </v-list>
+    <v-fab icon="mdi-plus" @click="addItemDialog = true" app></v-fab>
+  <v-dialog v-model="addItemDialog" max-width="600px">
+    <v-sheet>
+      <v-form @submit.prevent="addItem" class="d-flex flex-column pa-2">
+        <v-text-field v-model="newItemName" label="New item name" />
+        <v-text-field v-model.number="newItemValue" type="number" label="Value" />
+        <v-btn color="primary" type="submit">Add</v-btn>
+      </v-form>
+    </v-sheet>
+  </v-dialog>
+  </v-sheet>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import type { Item } from '../types';
 import { ItemAPI } from '../crud';
-
+import { VSheet, VList, VListItem, VBtn,  VFab, VDialog, VForm, VTextField, VSpacer } from 'vuetify/components';
 const items = ref<Item[]>([]);
 const newItemName = ref('');
 const newItemValue = ref(0);
+const addItemDialog = ref(false);
 
 async function addItem() {
   const name = newItemName.value.trim();
