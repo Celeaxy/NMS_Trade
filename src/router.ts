477f0fe48f9611675a5b8fa100bc423371f7de2b
Celeaxy <byt.es@mailbox.org>
2025-09-28 14:33:39 +0200

    Change router history to hash mode to work as SPA on gh pages

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import TradeOverview from './components/TradeOverview.vue';
 import Stations from './views/Stations.vue';
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(BASE_ROUTE),
+  history: createWebHashHistory(BASE_ROUTE),
   routes,
 });
 
